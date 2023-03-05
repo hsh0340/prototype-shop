712e92be0bb6f15280e6fcaefb4a2493b0b11b6b
hsh0340 <hsh0340@naver.com>
2023-03-05 19:41:53 +0900

    create header component

--- src/App.js
+++ src/App.js
@@ -1,3 +1,7 @@
+import Header from "./components/Header";
+import Prototypes from "./components/Prototypes";
+import Orders from "./components/Orders";
+import Footer from "./components/Footer";
 
 
 function App() {
